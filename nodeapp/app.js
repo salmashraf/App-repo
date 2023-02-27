0ecdb0a73ab39f69f42c4fc82e122edef3a4bdbc
salmashraf <119735396+salmashraf@users.noreply.github.com>
2023-02-27 17:01:37 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1 style="color: #2196F3;">Hello From Paula 🚀</h1>');
+  res.send('<h1 style="color: #2196F3;">Hello From Salma🚀</h1>');
 });
 
 app.listen(port, () => {
